b30198c0ff63b9ec366e013cca40fe03a8a119dc
madinabonus258-jpg <madinabonus258@gmail.com>
2026-07-17 12:48:38 +0500

    Add function to toggle book panel visibility

---
+++ script.js
@@ -0,0 +1,13 @@
+function openBook(button){
+
+    const row = button.closest(".book-row");
+
+    const book = row.querySelector(".book");
+
+    const panel = row.querySelector(".lesson-panel");
+
+    book.classList.toggle("active");
+
+    panel.classList.toggle("show");
+
+}
